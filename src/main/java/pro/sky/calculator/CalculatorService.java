9ee0e793b90a3b61fe7eb7186580b1ae99bc6fc6
demoB33 <vasilenko-s@list.ru>
2023-03-19 22:27:20 +0700

    Final commit

---
+++ src/main/java/pro/sky/calculator/CalculatorService.java
@@ -0,0 +1,8 @@
+package pro.sky.calculator;
+
+public interface CalculatorService {
+    public int sum(int num1, int num2);
+    public int min(int num1, int num2);
+    public int multi(int num1, int num2);
+    public int div(int num1, int num2);
+}
